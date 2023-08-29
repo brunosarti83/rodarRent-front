3d51176cec3072c79071f5ecc92a54863223c636
Gerson <gercentu777@gmail.com>
2023-08-29 18:07:39 -0300

    Creado cars y Filtrado

---
+++ Client/rodarRent/src/data.js
@@ -0,0 +1,155 @@
+const carData = [
+  {
+    id: '1',
+    brand: 'Toyota',
+    model: 'Corolla',
+    transmission: 'Automatic',
+    fuelType: 'Gasoline',
+    capacity: '2-5 passengers',
+    pricePerDay: 50,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '2',
+    brand: 'Ford',
+    model: 'Mustang',
+    transmission: 'Manual',
+    fuelType: 'Gasoline',
+    capacity: '2-5 passengers',
+    pricePerDay: 100,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '3',
+    brand: 'Honda',
+    model: 'Civic',
+    transmission: 'Automatic',
+    fuelType: 'Electric',
+    capacity: '6 or more passangers',
+    pricePerDay: 10,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '4',
+    brand: 'Chevrolet',
+    model: 'Camaro',
+    transmission: 'Manual',
+    fuelType: 'Gasoline',
+    capacity: '2-5 passengers',
+    pricePerDay: 120,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '5',
+    brand: 'Nissan',
+    model: 'GT-R',
+    transmission: 'Automatic',
+    fuelType: 'Gasoline',
+    capacity: '6 or more passangers',
+    pricePerDay: 20,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '6',
+    brand: 'Tesla',
+    model: 'Model 3',
+    transmission: 'Automatic',
+    fuelType: 'Electric',
+    capacity: '2-5 passengers',
+    pricePerDay: 50,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '7',
+    brand: 'Volkswagen',
+    model: 'Golf',
+    transmission: 'Manual',
+    fuelType: 'Electric',
+    capacity: '2-5 passengers',
+    pricePerDay: 30,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+   },
+  {
+    id: '8',
+    brand: 'Toyota',
+    model: 'RAV4',
+    transmission: 'Automatic',
+    fuelType: 'Hybrid',
+    capacity: '6 or more passangers',
+    pricePerDay: 80,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '9',
+    brand: 'Hyundai',
+    model: 'Tucson',
+    transmission: 'Automatic',
+    fuelType: 'Hybrid',
+    capacity: '6 or more passangers',
+    pricePerDay: 80,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '10',
+    brand: 'Ford',
+    model: 'Explorer',
+    transmission: 'Automatic',
+    fuelType: 'Diesel',
+    capacity: '6 or more passangers',
+    pricePerDay: 10,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+   },
+  {
+    id: '11',
+    brand: 'Jeep',
+    model: 'Wrangler',
+    transmission: 'Manual',
+    fuelType: 'Gasoline',
+    capacity: '6 or more passangers',
+    pricePerDay: 150,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+   },
+  {
+    id: '12',
+    brand: 'Mazda',
+    model: 'CX-5',
+    transmission: 'Automatic',
+    fuelType: 'Gasoline',
+    capacity: '2-5 passengers',
+    pricePerDay: 220,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+   },
+  {
+    id: '13',
+    brand: 'Kia',
+    model: 'Sportage',
+    transmission: 'Automatic',
+    fuelType: 'Electric',
+    capacity: '2-5 passengers',
+    pricePerDay: 75,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+   },
+  {
+    id: '14',
+    brand: 'Subaru',
+    model: 'Outback',
+    transmission: 'Automatic',
+    fuelType: 'Hybrid',
+    capacity: '6 or more passangers',
+    pricePerDay: 15,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  },
+  {
+    id: '15',
+    brand: 'Tesla',
+    model: 'Model S',
+    transmission: 'Automatic',
+    fuelType: 'Electric',
+    capacity: '2-5 passengers',
+    pricePerDay: 5,
+    imageUrl: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_12aa5deef3794cc4ad0dfcd88426ef17.jpg'
+  }
+];
+ 
+ export default carData;
+ 
