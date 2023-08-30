58989bd2744b12b469c13643e42f85a310b10e37
martin250187 <martin.gette.5@gmail.com>
2023-08-29 21:04:26 -0300

    View Login

--- Client/rodarRent/src/App.jsx
+++ Client/rodarRent/src/App.jsx
@@ -1,9 +1,12 @@
+import Login from "./components/Login/login";
+import "./index.css";
 function App() {
   return (
     <>
       <h1>Rodar Rent</h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/rodarRent/tailwind.config.js
+++ Client/rodarRent/tailwind.config.js
@@ -22,7 +22,16 @@ export default {
     },
     colors:{
       'blue':'#163C85',
-    }
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#fff',
+    },
   },
   plugins: [],
 }
