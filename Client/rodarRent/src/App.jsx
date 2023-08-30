9eb1e04e9d3f34e3abc95c44e84605361adbfddf
martin250187 <martin.gette.5@gmail.com>
2023-08-30 20:10:59 -0300

    Formulario Login y Register estilado

--- Client/rodarRent/src/App.jsx
+++ Client/rodarRent/src/App.jsx
@@ -1,5 +1,4 @@
 import CarList from "./components/Cars/Cars";
-import Login from "./views/Login/Login";
 import Register from "./views/Register/Register";
 import { Route } from "react-router-dom";
 import "./index.css";
