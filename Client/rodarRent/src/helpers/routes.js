b4397562e6c78f0fe8b3d995455bef6afc3115e3
Martin Terribile <juanmartterribile@gmail.com>
2023-08-29 22:08:26 -0300

    Añadido nav y enrutado de cars, se agregan tambien imagenes en assets

---
+++ Client/rodarRent/src/helpers/routes.js
@@ -0,0 +1,10 @@
+const routesHelper = {
+    landing: '/',
+    cars: '/cars',
+    login: '/login',
+    register: '/register',
+    aboutUs: '/aboutUs',
+    contact: '/contact'
+}
+
+export default routesHelper
