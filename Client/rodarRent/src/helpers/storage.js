ff572aff62302bc5e7bea76a5f4b1493f1ea6c5b
brunosarti83 <brunosarti.bs@gmail.com>
2023-09-08 18:42:07 -0300

    feat(utils/storage) added functions to get and set localStorage

--- Client/rodarRent/src/helpers/storage.js
+++ Client/rodarRent/src/helpers/storage.js
@@ -20,3 +20,26 @@ export function setSessionStorage (key, value) {
         console.error(error.message)
     }
 }
+
+
+export function getLocalStorage (key) {
+    try {
+        const item = window.localStorage.getItem(key)
+        if (item) {
+            return JSON.parse(item)
+        }
+        return undefined
+    } catch (error) {
+        return undefined
+    }
+}
+
+export function setlocalStorage (key, value) {
+    try {
+        const stringValue = JSON.stringify(value)
+        window.localStorage.setItem(key,stringValue)
+        return value
+    } catch (error) {
+        console.error(error.message)
+    }
+}
