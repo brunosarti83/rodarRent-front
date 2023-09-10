cfc002b9c800f1f7cd626224bfedbe78c2f14b8a
samuelbernal44 <samuelbernal44@gmail.com>
2023-09-10 13:41:09 -0500

    configuración para levantar el cliente en local

--- Client/rodarRent/src/helpers/routes.js
+++ Client/rodarRent/src/helpers/routes.js
@@ -1,28 +1,30 @@
 const routesHelper = {
-    baseFrontUrl: 'http://127.0.0.1:5173',
-    baseBackUrl: 'http://localhost:3001',
-    landing: '/',
-    cars: '/cars',
-    login: '/login',
-    authGoogle: '/auth/google',
-    register: '/register',
-    registerGoogle: '/google',
-    aboutUs: '/aboutUs',
-    contact: '/contact',
-    dashboardCustomer: '/customer/:id',
-    allCustomers: '/allCustomers',
-    allBookings: '/allBookings',
-    detail: '/cars/:id',
-    booking: '/booking',
-    admin: '/admin',
-    googleAuthAux: '/googleAuthAux',
-    editInfo: '/edit-customer/:id',
-    bookingByIdCustomer: '/myBookings',
-}
+  baseFrontUrl: 'http://localhost:5173',
+  baseBackUrl: 'http://localhost:3001',
+  landing: '/',
+  cars: '/cars',
+  login: '/login',
+  authGoogle: '/auth/google',
+  register: '/register',
+  registerGoogle: '/google',
+  aboutUs: '/aboutUs',
+  contact: '/contact',
+  dashboardCustomer: '/customer/:id',
+  allCustomers: '/allCustomers',
+  allBookings: '/allBookings',
+  detail: '/cars/:id',
+  booking: '/booking',
+  admin: '/admin',
+  googleAuthAux: '/googleAuthAux',
+  editInfo: '/edit-customer/:id',
+  bookingByIdCustomer: '/myBookings',
+};
 
 export const API_BASE_URL = 'http://localhost:3001';
 
-export const getCustomerDetailsUrl = (customerId) => `${API_BASE_URL}/customers/${customerId}`;
-export const getBookingsByIdCustomerUrl = (customerId) => `${API_BASE_URL}/booking/filter?CustomerId=${customerId}`;
+export const getCustomerDetailsUrl = (customerId) =>
+  `${API_BASE_URL}/customers/${customerId}`;
+export const getBookingsByIdCustomerUrl = (customerId) =>
+  `${API_BASE_URL}/booking/filter?CustomerId=${customerId}`;
 export const updateCustomerInfoUrl = () => `${API_BASE_URL}/customers`;
-export default routesHelper
+export default routesHelper;
