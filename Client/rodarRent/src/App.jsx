efd4ac79069f08af5eb8506986f979e2774f1581
brunosarti83 <brunosarti.bs@gmail.com>
2023-09-12 16:47:09 -0300

    deploy GoogleAuth import

--- Client/rodarRent/src/App.jsx
+++ Client/rodarRent/src/App.jsx
@@ -1,35 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import routesHelper from './helpers/routes';
-import Home from './views/Home/Home'
-import Nav from './components/Nav/Nav';
-import Landing from './views/Landing/Landing';
-import Login from './views/Login/login';
-import Register from './views/Register/Register';
-import Detail from './views/Detail/Detail'
-import { useState } from 'react'
-import Booking from './views/Booking/Booking';
-import Contact from './views/Contact/Contact'
-import AboutUs from './views/AboutUs/AboutUs'
-import CustomerList from './components/CustomerList/CustomerList';
-import CustomerDetail from './components/CustomerDetail/CustomerDetail';
-import BookingList from './components/BookingList/BookingList';
-import Admin from './views/Admin/Admin';
-//import GoogleAuthAux from './views/googleAuthAux/GoogleAuthAux';
-
-
+import { Route, Routes } from "react-router-dom";
+import routesHelper from "./helpers/routes";
+import Home from "./views/Home/Home";
+import Nav from "./components/Nav/Nav";
+import Landing from "./views/Landing/Landing";
+import Login from "./views/Login/login";
+import Register from "./views/Register/Register";
+import Detail from "./views/Detail/Detail";
+import { useState } from "react";
+import Booking from "./views/Booking/Booking";
+import Contact from "./views/Contact/Contact";
+import AboutUs from "./views/AboutUs/AboutUs";
+import CustomerList from "./components/CustomerList/CustomerList";
+import CustomerDetail from "./components/CustomerDetail/CustomerDetail";
+import BookingList from "./components/BookingList/BookingList";
+import Admin from "./views/Admin/Admin";
+import GoogleAuthAux from "./views/googleAuthAux/GoogleAuthAux";
 
 function App() {
-  const [darkMode, setDarkmode] = useState(true)
+  const [darkMode, setDarkmode] = useState(true);
 
   const toggleDarkMode = () => {
-    const element = document.documentElement
-    setDarkmode(!darkMode)
+    const element = document.documentElement;
+    setDarkmode(!darkMode);
     if (darkMode) {
-      element.classList.add('dark')
+      element.classList.add("dark");
     } else {
-      element.classList.remove('dark')
+      element.classList.remove("dark");
     }
-  }
+  };
 
   return (
     <div>
@@ -43,11 +41,14 @@ function App() {
         <Route path={routesHelper.booking} element={<Booking />} />
         <Route path={routesHelper.aboutUs} element={<AboutUs />} />
         <Route path={routesHelper.contact} element={<Contact />} />
-        <Route path={routesHelper.allCustomers} element = {<CustomerList/>}/>
-        <Route path={routesHelper.dashboardCustomer} element = {<CustomerDetail/>}/>
-        <Route path={routesHelper.allBookings} element = {<BookingList/>}/>
-        <Route path={routesHelper.admin} element={<Admin/>} />
-        {/* <Route path={routesHelper.googleAuthAux} element={<GoogleAuthAux/>} /> */}
+        <Route path={routesHelper.allCustomers} element={<CustomerList />} />
+        <Route
+          path={routesHelper.dashboardCustomer}
+          element={<CustomerDetail />}
+        />
+        <Route path={routesHelper.allBookings} element={<BookingList />} />
+        <Route path={routesHelper.admin} element={<Admin />} />
+        <Route path={routesHelper.googleAuthAux} element={<GoogleAuthAux />} />
       </Routes>
     </div>
   );
