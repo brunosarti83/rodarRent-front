f85993ce341e9d14387e816dd07be867d861b356
Martin Terribile <juanmartterribile@gmail.com>
2023-09-12 16:25:30 -0300

    fix provisorio laptop estilos

--- Client/rodarRent/src/components/CustomerInfo/CustomerInfo.jsx
+++ Client/rodarRent/src/components/CustomerInfo/CustomerInfo.jsx
@@ -3,12 +3,12 @@ const CustomerInfo = ({ customer }) => {
   return (
     <div className="w-full p-4">
       <div className='border-b-2 border-gray-300' >
-        <h1 className=" text-2xl font-medium text-black font-poppins">
+        <h1 className=" text-2xl font-medium text-black font-poppins dark:text-gray-100">
           Personal Info
         </h1>
       </div>
-      <div className="w-full rounded-lg  bg-white border border-gray-300 drop-shadow-md mt-10 ">
-        <div className="grid grid-cols-3 grid-rows-3 gap-3">
+      <div className="w-full rounded-lg  bg-white border border-gray-300 drop-shadow-md mt-10 dark:bg-slate-900 dark:border-none ">
+        <div className="grid grid-cols-3 grid-rows-3 gap-3 ">
           {[
             { label: 'Name', value: customer.name },
             { label: 'Last Name', value: customer.lastName },
@@ -20,8 +20,8 @@ const CustomerInfo = ({ customer }) => {
             { label: 'Zip Code', value: customer.zipCode },
             { label: 'Phone', value: customer.phoneNumber },
           ].map(({ label, value }) => (
-            <div key={label} className="w-full h-13 px-6 py-6 bg-white rounded-md shadow">
-              <p className="font-poppins text-black text-16 font-normal">
+            <div key={label} className="w-full h-13 px-6 py-6 bg-white dark:bg-slate-950  rounded-md shadow">
+              <p className="font-poppins text-black dark:text-gray-100 text-16 font-normal">
                 {label}: {value}
               </p>
             </div>
--- Client/rodarRent/src/components/DashboardActions/DashboardActions.jsx
+++ Client/rodarRent/src/components/DashboardActions/DashboardActions.jsx
@@ -8,18 +8,18 @@ const DashboardActions = ({ openEditModal }) => {
       <div className="w-full h-2/3">
         <img src={DashboardActionsImg} alt="Dashboard" className="w-full h-full object-cover rounded-t-lg" />
       </div>
-      <div className='h-1/3 w-full bg-white drop-shadow-md p-4 border border-gray-300 rounded-b-lg'>
+      <div className='h-1/3 w-full bg-white drop-shadow-md p-4 border border-gray-300 rounded-b-lg dark:bg-slate-900'>
         <h2 className="text-lg font-bold text-left">Actions</h2>
         <hr className="border border-gray-300" />
         <ul className="flex flex-col items-center h-3/4 mt-4 justify-evenly">
           <li>
-            <Link to="/subscribe" className="text-blue-600 py-1 px-8 rounded-lg bg-white drop-shadow-lg border border-gray-300 hover:drop-shadow-none">Subscribe to our newsletter</Link>
+            <Link to="/subscribe" className="text-blue-600 py-1 px-8 rounded-lg bg-white drop-shadow-lg border border-gray-300 dark:bg-slate-950 hover:drop-shadow-none">Subscribe to our newsletter</Link>
           </li>
           <li>
-            <Link to="/review" className="py-1 px-8 rounded-lg bg-white drop-shadow-lg text-blue-600 border border-gray-300 hover:drop-shadow-none">Give us your review</Link>
+            <Link to="/review" className="py-1 px-8 rounded-lg bg-white drop-shadow-lg text-blue-600 border border-gray-300 dark:bg-slate-950 hover:drop-shadow-none">Give us your review</Link>
           </li>
           <li>
-            <button onClick={openEditModal} className="py-1 px-8 rounded-lg bg-white drop-shadow-lg text-blue-600 border border-gray-300 hover:drop-shadow-none">Edit your personal info</button>
+            <button onClick={openEditModal} className="py-1 px-8 rounded-lg bg-white drop-shadow-lg text-blue-600 border border-gray-300 dark:bg-slate-950 hover:drop-shadow-none">Edit your personal info</button>
           </li>
         </ul>
       </div>
