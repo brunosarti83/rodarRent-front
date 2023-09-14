996a3c2d601c7ced17dd9352ab3b3545550f2931
Delta-39 <martinterribile@gmail.com>
2023-09-14 11:49:09 -0300

    add hamburguer menu

--- Client/rodarRent/src/components/Nav/Nav.jsx
+++ Client/rodarRent/src/components/Nav/Nav.jsx
@@ -1,45 +1,116 @@
 /* eslint-disable react/prop-types */
-import { Link, useLocation } from "react-router-dom"
-import routesHelper from "../../helpers/routes"
-import { GiCarKey } from "react-icons/gi"
-import { BiSun, BiMoon, BiMenu } from "react-icons/bi"
-import { useState } from "react"
+import { Link, useLocation } from "react-router-dom";
+import routesHelper from "../../helpers/routes";
+import { GiCarKey } from "react-icons/gi";
+import { BiSun, BiMoon } from "react-icons/bi";
+import { useState } from "react";
 
 function Nav(props) {
+  const { darkMode, toggleDarkMode } = props;
 
-    const {darkMode, toggleDarkMode} = props
-    
-    const [dropMenu,setDropMenu] = useState(false)
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const handleDropMenu = () =>{
-        setDropMenu(!dropMenu)
-    }
+  const navItems = [
+    { name: "About us", link: routesHelper.aboutUs },
+    { name: "Cars", link: routesHelper.cars },
+    { name: "Contact", link: routesHelper.contact },
+  ];
 
-    const location = useLocation()
-    const isLanding = location.pathname !== '/'
+  const location = useLocation();
+  const isLanding = location.pathname !== "/";
 
-
-    return (
-        <header className="flex px-16 py-2 justify-between font-poppins bg-white dark:bg-slate-900 dark:text-gray-100 transition duration-300" >
-            <div className="flex items-center" >
-                <GiCarKey size='50px' />
-                <h2 className=" ml-4 text-2xl">
-                    {isLanding ? <Link to={routesHelper.landing}>RodarRent</Link> :'RodarRent'}
-                </h2>
+  return (
+    <header className="flex px-16 py-2 justify-between items-center font-poppins bg-white dark:bg-slate-900 dark:text-gray-100 transition duration-300">
+      <div className="flex items-center">
+        <GiCarKey size="50px" />
+        <h2 className=" ml-4 text-2xl">
+          {isLanding ? (
+            <Link to={routesHelper.landing}>RodarRent</Link>
+          ) : (
+            "RodarRent"
+          )}
+        </h2>
+      </div>
+      <nav>
+        <section className="MOBILE-MENU items-center flex lg:hidden">
+        <div
+            onClick={toggleDarkMode}
+            className="rounded-md drop-shadow-md mr-7 bg-white dark:bg-slate-900 cursor-pointer p-1 ease-in-out hover:drop-shadow-none "
+          >
+            {darkMode ? <BiSun size="30px" /> : <BiMoon size="30px" />}
+          </div>
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 bg-gray-600"></span>
+            <span className="block h-0.5 w-8 bg-gray-600"></span>
+            <span className="block h-0.5 w-8 bg-gray-600"></span>
+          </div>         
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
             </div>
-            <nav className="flex items-center">
-                <BiMenu onClick={handleDropMenu} size='50px' className=" 2xl:hidden xl:hidden lg:hidden md:block sm:block" />
-                <ul className={`text-xl ${dropMenu ? 'hidden':'flex'}`} >
-                    <li className="mr-14" ><Link to={routesHelper.aboutUs}>About Us</Link></li>
-                    <li className="mr-14" ><Link to={routesHelper.cars}>Cars</Link></li>
-                    <li className="mr-14" ><Link to={routesHelper.contact}>Contact</Link></li>
-                </ul>
-                <div onClick={toggleDarkMode} className="rounded-md drop-shadow-md bg-white dark:bg-slate-900 cursor-pointer p-1 ease-in-out hover:drop-shadow-none " >
-                    {darkMode ? <BiSun size='30px'/> : <BiMoon size='30px'/>}
-                </div>
-            </nav>
-        </header>
-    )
+            <ul className="flex flex-col text-xl items-center justify-between min-h-[250px]">
+              {navItems.map((item, index) => (
+                <li className="my-8" key={index}>
+                  <Link to={item.link}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+        <div className="flex items-center " >
+            <ul className="DESKTOP-MENU hidden text-xl space-x-8 lg:flex">
+            {navItems.map((item, index) => (
+                <li className="my-8" key={index}>
+                <Link to={item.link}>{item.name}</Link>
+                </li>
+            ))}
+            </ul>
+            <div
+            onClick={toggleDarkMode}
+            className=" ml-7 rounded-md drop-shadow-md bg-white dark:bg-slate-900 cursor-pointer p-1 ease-in-out hover:drop-shadow-none sm:hidden md:hidden lg:block xl:block 2xl:block "
+            >
+            {darkMode ? <BiSun size="30px" /> : <BiMoon size="30px" />}
+            </div>
+        </div>        
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+    </header>
+  );
 }
 
-export default Nav
+export default Nav;
