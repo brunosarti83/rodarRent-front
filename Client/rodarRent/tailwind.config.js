0b712352aa482c6e925c0c22f350a184499c56a4
Martin Terribile <juanmartterribile@gmail.com>
2023-09-13 21:17:05 -0300

    add(/landing responsive styles partial)

--- Client/rodarRent/tailwind.config.js
+++ Client/rodarRent/tailwind.config.js
@@ -8,12 +8,20 @@ export default {
   theme: {
     extend: {
       fontSize:{
-        landingDesktop:'5.4rem',
-        landingLaptop:'4.4rem'
+        landing2Xl:'5rem',
+        landingXl:'4.5rem',
+        landingLg: '3.8rem',
+        landingMd:'3.5rem',
+      },
+      scale:{
+        '175':'1.75',
+        '185':'1.85',
+        '200':'2',
+        '215':'2.15'
       },
       height:{
-        landingDesktop:'770px',
-        landingLaptop:'560px',
+        landingMd:'70vh',
+        landingLaptop:'80vh',
         noNavDesktop:'93.1vh',
         noNavLaptop:'99.1vh',
         customerDetail:'86.5vh',
