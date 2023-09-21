4b006f7c33e8c0a063b408502f06cc5ce9b0fa26
Martin Terribile <juanmartterribile@gmail.com>
2023-09-20 21:46:28 -0300

    commit para checkout

--- Client/rodarRent/src/components/AdminComponents/AdminVehicles/EditVehicle/EditVehicle.jsx
+++ Client/rodarRent/src/components/AdminComponents/AdminVehicles/EditVehicle/EditVehicle.jsx
@@ -1,24 +1,38 @@
 import axios from "axios"
 import { API_BASE_URL } from "../../../../helpers/routes"
 import { useEffect, useState } from "react"
+//Components
+import Loader from "../../../Loader/Loader"
 
 const EditVehicle = ({selectedVehicle}) =>{
 
     const [vehicle,setVehicle] = useState(null)
+    const [loading,setLoading] = useState(false)
+
     const {id} = selectedVehicle
 
     useEffect(()=>{
-        axios
-            .get(`${API_BASE_URL}/vehicles/${id}`)
-            .then((response)=>{
-                setVehicle(response)
-            })
+        loading || setLoading(true)
+        const fetchVehicleData = async () =>{
+            try {
+                const response = await axios.get(`${API_BASE_URL}/vehicles/${id}`)
+                setVehicle(response.data)
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+            }
+        }
 
-    })
+    },[id])
     
+    console.log(vehicle)
     return(
         <div>
+            {loading ? (
+                <Loader />
+            ):(
 
+            )}
         </div>
     )
 }
