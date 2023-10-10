bc75ec2593a92efdbabc8ef1d8c76ef975ab5610
brunosarti83 <brunosarti.bs@gmail.com>
2023-10-10 19:55:05 -0300

    carrousel dark mode

--- Client/rodarRent/src/views/Landing/Carrousel.jsx
+++ Client/rodarRent/src/views/Landing/Carrousel.jsx
@@ -1,19 +1,19 @@
-import ford from '../../assets/img/brandsLogos/ford.svg';
-import jeep from '../../assets/img/brandsLogos/jeep.svg';
-import nissan from '../../assets/img/brandsLogos/nissan.svg';
-import chevrolet from '../../assets/img/brandsLogos/chevrolet.png';
-import honda from '../../assets/img/brandsLogos/honda.png';
-import hyundai from '../../assets/img/brandsLogos/hyundai.png';
-import mercedes from '../../assets/img/brandsLogos/mercedes.png';
-import tesla from '../../assets/img/brandsLogos/tesla.png';
-import toyota from '../../assets/img/brandsLogos/toyota.png';
-import vw from '../../assets/img/brandsLogos/vw.png';
-import audi from '../../assets/img/brandsLogos/audi.svg';
-import fiat from '../../assets/img/brandsLogos/fiat.svg';
+import ford from "../../assets/img/brandsLogos/ford.svg";
+import jeep from "../../assets/img/brandsLogos/jeep.svg";
+import nissan from "../../assets/img/brandsLogos/nissan.svg";
+import chevrolet from "../../assets/img/brandsLogos/chevrolet.png";
+import honda from "../../assets/img/brandsLogos/honda.png";
+import hyundai from "../../assets/img/brandsLogos/hyundai.png";
+import mercedes from "../../assets/img/brandsLogos/mercedes.png";
+import tesla from "../../assets/img/brandsLogos/tesla.png";
+import toyota from "../../assets/img/brandsLogos/toyota.png";
+import vw from "../../assets/img/brandsLogos/vw.png";
+import audi from "../../assets/img/brandsLogos/audi.svg";
+import fiat from "../../assets/img/brandsLogos/fiat.svg";
 
 function Carrousel() {
   return (
-    <div className="bg-white overflow-hidden py-2">
+    <div className="bg-white overflow-hidden py-2 dark:bg-slate-900">
       <div className="animate-slide-horizontal">
         <div className="flex items-center justify-center space-x-20">
           <img className="w-20 h-20" src={ford} alt={ford} />
